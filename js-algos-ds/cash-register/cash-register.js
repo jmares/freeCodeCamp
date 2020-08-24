d587c3c8871251f0fbf9e8545450d2a87945a2b0
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-08-24 16:53:28 +0200

    JS: cash register 2 of 4 scenarios accounted for

--- js-algos-ds/cash-register/cash-register.js
+++ js-algos-ds/cash-register/cash-register.js
@@ -1,6 +1,50 @@
-function checkCashRegister(price, cash, cid) {
-    var change;
-    return change;
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
+function cashInDrawer(cid) {
+  let amount = 0;
+  for (let i = 0; i < cid.length; i++) {
+    amount += cid[i][1];
   }
+  return amount;
+}
+
+function checkCashRegister(price, cash, cid) {
+  let change = cash - price;
+  let output = { status: null, change: [] };
+  let amountCID = cashInDrawer(cid);
+  //console.log("amountCID = " + amountCID + "| change = " + change);
+  if (amountCID < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+  } else if ( amountCID == change) {
+    output.status = "CLOSED";
+    output.change = cid;
+  } else {
+    let revCID = cid.reverse();
+    let calcChange = change;
+    for (let i = 0; i < denom.length; i++) {
+      if ((calcChange > denom[i].value) && revCID[i][1] > 0) {
+        
+      } 
+      console.log(denom[i]);
+    }
+    output.status = "BUSY";
+    output.change = [0];
+    console.log(revCid);
+  }
+  return output;
+}
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
