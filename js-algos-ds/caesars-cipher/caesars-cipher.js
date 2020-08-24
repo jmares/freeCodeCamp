eed428f23004524cd36e700b10441a3a9a951e16
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-08-24 12:29:11 +0200

    JS: Caesars Cipher finished

--- js-algos-ds/caesars-cipher/caesars-cipher.js
+++ js-algos-ds/caesars-cipher/caesars-cipher.js
@@ -1,8 +1,21 @@
 function rot13(str) {
-
-    return str;
-  }
+    let newArr = [];
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (code < 65 || code > 90) {
+            newArr.push(str[i]);
+        } else if (code < 78) {
+            newArr.push(String.fromCharCode(code + 13)) ;
+        } else  {
+            newArr.push(String.fromCharCode(code - 13)) ;
+        }
+    }
+    return newArr.join("");
+}
   
   console.log(rot13("SERR PBQR PNZC"));
+  console.log(rot13("SERR CVMMN!"));
+  console.log(rot13("SERR YBIR?"));
+  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
 
   
