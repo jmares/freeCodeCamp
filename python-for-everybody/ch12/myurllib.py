0ff7d617d2301c94af13929960a56e29257de3fd
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-09-10 14:26:15 +0200

    added site to myurllib.py

--- python-for-everybody/ch12/myurllib.py
+++ python-for-everybody/ch12/myurllib.py
@@ -1,7 +1,8 @@
 import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pre4.org/romeo.txt')
+#fhand = urllib.request.urlopen('http://data.pre4.org/romeo.txt')
 #fhand = urllib.request.urlopen('http://www.pannix.net/index.html')
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
 
 for line in fhand:
     print(line.decode().strip())
