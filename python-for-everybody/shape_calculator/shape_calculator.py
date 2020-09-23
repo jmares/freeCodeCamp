0e4d4a655f425e3d096e3586c5eb9588ee4102a6
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-09-23 09:44:18 +0200

    shape calculator, square finished and tested, except get_amount_inside

--- python-for-everybody/shape_calculator/shape_calculator.py
+++ python-for-everybody/shape_calculator/shape_calculator.py
@@ -19,7 +19,7 @@ class Rectangle:
         return 2 * (self.width + self.height)
     
     def get_diagonal(self):
-        return (self.width ** 2 + self.heigh ** 2) ** .5
+        return (self.width ** 2 + self.height ** 2) ** .5
         
     def get_picture(self):
         if self.height > 50 or self.width > 50:
@@ -30,4 +30,20 @@ class Rectangle:
     def get_amount_inside(self, shape):
         pass
 
-#class Square:
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+    
+    def __repr__(self):
+        return f"Square(side={self.width})"
+    
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+    
+    def set_width(self, side):
+        self.set_side(side)
+
+    def set_height(self, side):
+        self.set_side(side)
+    
