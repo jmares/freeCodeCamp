3fd58a992c795aab4e0fd57018758ec4fcd7773c
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-09-23 09:36:14 +0200

    shape calculator, rectangle finished and tested, except get_amount_inside

--- python-for-everybody/shape_calculator/shape_calculator.py
+++ python-for-everybody/shape_calculator/shape_calculator.py
@@ -1,6 +1,33 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def __repr__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
+    def set_width(self, width):
+        self.width = width
 
+    def set_height(self, height):
+        self.height = height
+    
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.heigh ** 2) ** .5
+        
+    def get_picture(self):
+        if self.height > 50 or self.width > 50:
+            return "Too big for picture."
+        else:
+            return ("*" * self.width + "\n") * self.height
 
-class Square:
+    def get_amount_inside(self, shape):
+        pass
+
+#class Square:
