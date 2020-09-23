6c7b15a741637e6af73b2111d9dcdca261e2e9a4
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-09-23 16:24:15 +0200

    probability calculator, class hat finished and tested

--- python-for-everybody/probability_calculator/prob_calculator.py
+++ python-for-everybody/probability_calculator/prob_calculator.py
@@ -2,7 +2,26 @@ import copy
 import random
 # Consider using the modules imported above.
 
-class Hat:
+class Hat(object):
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items():
+            self.contents.extend([key] * value)
+        random.shuffle(self.contents)
+
+    def draw(self, number):
+        drawn = []
+        if number >= len(self.contents):
+            drawn = copy.deepcopy(self.contents)
+            random.shuffle(drawn)
+            self.contents = []
+        else:
+            while len(drawn) < number:
+                r = random.randint(0, len(self.contents) - 1)
+                drawn.append(self.contents[r])
+                del self.contents[r]
+        return drawn
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
